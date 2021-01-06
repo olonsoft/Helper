6c57f843959ce0b686727c713a6d78edf05802cb
Dimitris Paterakis <dimitris19@gmail.com>
2021-01-06 20:02:36 +0200

    202101062002

--- src/helper_debug.h
+++ src/helper_debug.h
@@ -19,7 +19,10 @@
   #define APP_LOG_LEVEL      4
 #endif
 
-#define TERMINAL_IN_COLOR
+#ifndef TERMINAL_IN_COLOR
+//#define TERMINAL_IN_COLOR
+#endif
+
 // to print color ansi chars on platformio terminal 
 // set monitor_flags  = --raw in platformio.ini
 // https://en.wikipedia.org/wiki/ANSI_escape_code
@@ -36,25 +39,25 @@
 #define TERMINAL_COLOR_FUNCTION "\e[35m" // magenta
 
 #ifdef TERMINAL_IN_COLOR
-#define TERMINAL_COLOR_ERROR    "[" TERMINAL_COLOR_RED "E" TERMINAL_COLOR_RESET "] "
-#define TERMINAL_COLOR_WARN     "[" TERMINAL_COLOR_YELLOW "W" TERMINAL_COLOR_RESET "] "
-#define TERMINAL_COLOR_INFO     "[" TERMINAL_COLOR_GREEN "I" TERMINAL_COLOR_RESET "] "
-#define TERMINAL_COLOR_DEBUG    "[" TERMINAL_COLOR_BLUE "D" TERMINAL_COLOR_RESET "] "
-#define TERMINAL_COLOR_VERBOSE  "[" TERMINAL_COLOR_CYAN "V" TERMINAL_COLOR_RESET "] "
+#define TERMINAL_COLOR_ERROR    DBG_PORT.print("[" TERMINAL_COLOR_RED "E" TERMINAL_COLOR_RESET "] ")
+#define TERMINAL_COLOR_WARN     DBG_PORT.print("[" TERMINAL_COLOR_YELLOW "W" TERMINAL_COLOR_RESET "] ")
+#define TERMINAL_COLOR_INFO     DBG_PORT.print("[" TERMINAL_COLOR_GREEN "I" TERMINAL_COLOR_RESET "] ")
+#define TERMINAL_COLOR_DEBUG    DBG_PORT.print("[" TERMINAL_COLOR_BLUE "D" TERMINAL_COLOR_RESET "] ")
+#define TERMINAL_COLOR_VERBOSE  DBG_PORT.print("[" TERMINAL_COLOR_CYAN "V" TERMINAL_COLOR_RESET "] ")
 
 #ifdef TERMINAL_TIME_PRINT
 #define TIMESTAMP_PRINT      DBG_PORT.printf("[" TERMINAL_COLOR_TIME "%s" TERMINAL_COLOR_RESET "] ", timeToString().c_str())
 #else
-#define TIMESTAMP_PRINT      DBG_PORT.printf(("[" TERMINAL_COLOR_TIME "%10lu" TERMINAL_COLOR_RESET "] ", millis())
+#define TIMESTAMP_PRINT      DBG_PORT.printf("[" TERMINAL_COLOR_TIME "%10lu" TERMINAL_COLOR_RESET "] ", millis())
 #endif
 
 #else
 
-#define TERMINAL_COLOR_ERROR "[E] "
-#define TERMINAL_COLOR_WARN  "[W] "
-#define TERMINAL_COLOR_INFO  "[I] "
-#define TERMINAL_COLOR_DEBUG "[D] "
-#define TERMINAL_COLOR_DEBUG "[V] "
+#define TERMINAL_COLOR_ERROR   DBG_PORT.print("[E] ")
+#define TERMINAL_COLOR_WARN    DBG_PORT.print("[W] ")
+#define TERMINAL_COLOR_INFO    DBG_PORT.print("[I] ")
+#define TERMINAL_COLOR_DEBUG   DBG_PORT.print("[D] ")
+#define TERMINAL_COLOR_VERBOSE DBG_PORT.print("[V] ")
 #ifdef TERMINAL_TIME_PRINT
 #define TIMESTAMP_PRINT      DBG_PORT.printf("[%s] ", timeToString().c_str())
 #else
@@ -63,47 +66,60 @@
 #endif
 
 // todo: use __PRETTY_FUNCTION__ , __FILE__ , __LINE__ , __func__
+// simple New Line
+#define LOGLN             if (APP_LOG_LEVEL>0) { DBG_PORT.println(); }
 
 // log errors
-#define LOGERROR(x)       if (APP_LOG_LEVEL>0) { DBG_PORT.print(x); }
-#define LOGERRORLN(x)     if (APP_LOG_LEVEL>0) { DBG_PORT.println(x); }
-#define LOGERRORF(...)    if (APP_LOG_LEVEL>0) { DBG_PORT.printf(__VA_ARGS__); }
-#define LOGERRORF_P(...)  if (APP_LOG_LEVEL>0) { DBG_PORT.printf_P(__VA_ARGS__); }
+#define LOGERROR(x)       if (APP_LOG_LEVEL>0) { TERMINAL_COLOR_ERROR; DBG_PORT.print(x); }
+#define LOGERRORLN(x)     if (APP_LOG_LEVEL>0) { TERMINAL_COLOR_ERROR; DBG_PORT.println(x); }
+#define LOGERRORF(...)    if (APP_LOG_LEVEL>0) { TERMINAL_COLOR_ERROR; DBG_PORT.printf(__VA_ARGS__); }
+#define LOGERRORF_P(...)  if (APP_LOG_LEVEL>0) { TERMINAL_COLOR_ERROR; DBG_PORT.printf_P(__VA_ARGS__); }
 // log errors with time stamp
-#define TLOGERROR(x)      if (APP_LOG_LEVEL>0) { TIMESTAMP_PRINT; LOGERROR(x); }
-#define TLOGERRORLN(x)    if (APP_LOG_LEVEL>0) { TIMESTAMP_PRINT; LOGERRORLN(x);  }
-#define TLOGERRORF(...)   if (APP_LOG_LEVEL>0) { TIMESTAMP_PRINT; LOGERRORF(...); }
-#define TLOGERRORF_P(...) if (APP_LOG_LEVEL>0) { TIMESTAMP_PRINT; LOGERRORF_P(...); }
+#define TLOGERROR(x)      if (APP_LOG_LEVEL>0) { TERMINAL_COLOR_ERROR; TIMESTAMP_PRINT; DBG_PORT.print(x); }
+#define TLOGERRORLN(x)    if (APP_LOG_LEVEL>0) { TERMINAL_COLOR_ERROR; TIMESTAMP_PRINT; DBG_PORT.println(x);  }
+#define TLOGERRORF(...)   if (APP_LOG_LEVEL>0) { TERMINAL_COLOR_ERROR; TIMESTAMP_PRINT; DBG_PORT.printf(__VA_ARGS__); }
+#define TLOGERRORF_P(...) if (APP_LOG_LEVEL>0) { TERMINAL_COLOR_ERROR; TIMESTAMP_PRINT; DBG_PORT.printf_P(__VA_ARGS__); }
 
 // log warnings
-#define LOGWARN(x)        if (APP_LOG_LEVEL>1) { DBG_PORT.print(x); }
-#define LOGWARNLN(x)      if (APP_LOG_LEVEL>1) { DBG_PORT.println(x); }
-#define LOGWARNF(...)     if (APP_LOG_LEVEL>1) { DBG_PORT.printf(__VA_ARGS__); }
-#define LOGWARNF_P(...)   if (APP_LOG_LEVEL>1) { DBG_PORT.printf_P(__VA_ARGS__); }
+#define LOGWARN(x)        if (APP_LOG_LEVEL>1) { TERMINAL_COLOR_WARN; DBG_PORT.print(x); }
+#define LOGWARNLN(x)      if (APP_LOG_LEVEL>1) { TERMINAL_COLOR_WARN; DBG_PORT.println(x); }
+#define LOGWARNF(...)     if (APP_LOG_LEVEL>1) { TERMINAL_COLOR_WARN; DBG_PORT.printf(__VA_ARGS__); }
+#define LOGWARNF_P(...)   if (APP_LOG_LEVEL>1) { TERMINAL_COLOR_WARN; DBG_PORT.printf_P(__VA_ARGS__); }
 // log wanrings with timestamp
-#define TLOGWARN(x)       if (APP_LOG_LEVEL>1) { TIMESTAMP_PRINT; LOGWARN(x); }
-#define TLOGWARNLN(x)     if (APP_LOG_LEVEL>1) { TIMESTAMP_PRINT; LOGWARNLN(x);  }
-#define TLOGWARNF(...)    if (APP_LOG_LEVEL>1) { TIMESTAMP_PRINT; LOGWARNF(...); }
-#define TLOGWARNF_P(...)  if (APP_LOG_LEVEL>1) { TIMESTAMP_PRINT; LOGWARNF_P(...); }
+#define TLOGWARN(x)       if (APP_LOG_LEVEL>1) { TERMINAL_COLOR_WARN; TIMESTAMP_PRINT; DBG_PORT.print(x); }
+#define TLOGWARNLN(x)     if (APP_LOG_LEVEL>1) { TERMINAL_COLOR_WARN; TIMESTAMP_PRINT; DBG_PORT.println(x);  }
+#define TLOGWARNF(...)    if (APP_LOG_LEVEL>1) { TERMINAL_COLOR_WARN; TIMESTAMP_PRINT; DBG_PORT.printf(__VA_ARGS__); }
+#define TLOGWARNF_P(...)  if (APP_LOG_LEVEL>1) { TERMINAL_COLOR_WARN; TIMESTAMP_PRINT; DBG_PORT.printf_P(__VA_ARGS__); }
 
 //log info
-#define LOGINFO(x)        if (APP_LOG_LEVEL>2) { DBG_PORT.print(x); }
-#define LOGINFOLN(x)      if (APP_LOG_LEVEL>2) { DBG_PORT.println(x); }
-#define LOGINFOF(...)     if (APP_LOG_LEVEL>2) { DBG_PORT.printf(__VA_ARGS__); }
-#define LOGINFOF_P(...)   if (APP_LOG_LEVEL>2) { DBG_PORT.printf_P(__VA_ARGS__); }
+#define LOGINFO(x)        if (APP_LOG_LEVEL>2) { TERMINAL_COLOR_INFO; DBG_PORT.print(x); }
+#define LOGINFOLN(x)      if (APP_LOG_LEVEL>2) { TERMINAL_COLOR_INFO; DBG_PORT.println(x); }
+#define LOGINFOF(...)     if (APP_LOG_LEVEL>2) { TERMINAL_COLOR_INFO; DBG_PORT.printf(__VA_ARGS__); }
+#define LOGINFOF_P(...)   if (APP_LOG_LEVEL>2) { TERMINAL_COLOR_INFO; DBG_PORT.printf_P(__VA_ARGS__); }
 // log info with timestamp
-#define TLOGINFO(x)       if (APP_LOG_LEVEL>2) { TIMESTAMP_PRINT; LOGINFO(x); }
-#define TLOGINFOLN(x)     if (APP_LOG_LEVEL>2) { TIMESTAMP_PRINT; LOGINFOLN(x);  }
-#define TLOGINFOF(...)    if (APP_LOG_LEVEL>2) { TIMESTAMP_PRINT; LOGINFOF(...); }
-#define TLOGINFOF_P(...)  if (APP_LOG_LEVEL>2) { TIMESTAMP_PRINT; LOGINFOF_P(...); }
+#define TLOGINFO(x)       if (APP_LOG_LEVEL>2) { TERMINAL_COLOR_INFO; TIMESTAMP_PRINT; DBG_PORT.print(x); }
+#define TLOGINFOLN(x)     if (APP_LOG_LEVEL>2) { TERMINAL_COLOR_INFO; TIMESTAMP_PRINT; DBG_PORT.println(x);  }
+#define TLOGINFOF(...)    if (APP_LOG_LEVEL>2) { TERMINAL_COLOR_INFO; TIMESTAMP_PRINT; DBG_PORT.printf(__VA_ARGS__); }
+#define TLOGINFOF_P(...)  if (APP_LOG_LEVEL>2) { TERMINAL_COLOR_INFO; TIMESTAMP_PRINT; DBG_PORT.printf_P(__VA_ARGS__); }
 
 //log debug
-#define LOGDEBUG(x)       if (APP_LOG_LEVEL>3) { DBG_PORT.print(x); }
-#define LOGDEBUGLN(x)     if (APP_LOG_LEVEL>3) { DBG_PORT.println(x); }
-#define LOGDEBUGF(...)    if (APP_LOG_LEVEL>3) { DBG_PORT.printf(__VA_ARGS__); }
-#define LOGDEBUGF_P(...)  if (APP_LOG_LEVEL>3) { DBG_PORT.printf_P(__VA_ARGS__); }
+#define LOGDEBUG(x)       if (APP_LOG_LEVEL>3) { TERMINAL_COLOR_DEBUG; DBG_PORT.print(x); }
+#define LOGDEBUGLN(x)     if (APP_LOG_LEVEL>3) { TERMINAL_COLOR_DEBUG; DBG_PORT.println(x); }
+#define LOGDEBUGF(...)    if (APP_LOG_LEVEL>3) { TERMINAL_COLOR_DEBUG; DBG_PORT.printf(__VA_ARGS__); }
+#define LOGDEBUGF_P(...)  if (APP_LOG_LEVEL>3) { TERMINAL_COLOR_DEBUG; DBG_PORT.printf_P(__VA_ARGS__); }
 //log debug with timestamp
-#define TLOGDEBUG(x)      if (APP_LOG_LEVEL>3) { TIMESTAMP_PRINT; LOGDEBUG(x); }
-#define TLOGDEBUGLN(x)    if (APP_LOG_LEVEL>3) { TIMESTAMP_PRINT; LOGDEBUGLN(x);  }
-#define TLOGDEBUGF(...)   if (APP_LOG_LEVEL>3) { TIMESTAMP_PRINT; LOGDEBUGF(...); }
-#define TLOGDEBUGF_P(...) if (APP_LOG_LEVEL>3) { TIMESTAMP_PRINT; LOGDEBUGF_P(...); }
+#define TLOGDEBUG(x)      if (APP_LOG_LEVEL>3) { TERMINAL_COLOR_DEBUG; TIMESTAMP_PRINT; DBG_PORT.print(x); }
+#define TLOGDEBUGLN(x)    if (APP_LOG_LEVEL>3) { TERMINAL_COLOR_DEBUG; TIMESTAMP_PRINT; DBG_PORT.println(x);  }
+#define TLOGDEBUGF(...)   if (APP_LOG_LEVEL>3) { TERMINAL_COLOR_DEBUG; TIMESTAMP_PRINT; DBG_PORT.printf(__VA_ARGS__); }
+#define TLOGDEBUGF_P(...) if (APP_LOG_LEVEL>3) { TERMINAL_COLOR_DEBUG; TIMESTAMP_PRINT; DBG_PORT.printf_P(__VA_ARGS__); }
+
+//log verbose
+#define LOGVERBOSE(x)       if (APP_LOG_LEVEL>4) { TERMINAL_COLOR_VERBOSE; DBG_PORT.print(x); }
+#define LOGVERBOSELN(x)     if (APP_LOG_LEVEL>4) { TERMINAL_COLOR_VERBOSE; DBG_PORT.println(x); }
+#define LOGVERBOSEF(...)    if (APP_LOG_LEVEL>4) { TERMINAL_COLOR_VERBOSE; DBG_PORT.printf(__VA_ARGS__); }
+#define LOGVERBOSEF_P(...)  if (APP_LOG_LEVEL>4) { TERMINAL_COLOR_VERBOSE; DBG_PORT.printf_P(__VA_ARGS__); }
+//log verbose with timestamp
+#define TLOGVERBOSE(x)      if (APP_LOG_LEVEL>4) { TERMINAL_COLOR_VERBOSE; TIMESTAMP_PRINT; DBG_PORT.print(x); }
+#define TLOGVERBOSELN(x)    if (APP_LOG_LEVEL>4) { TERMINAL_COLOR_VERBOSE; TIMESTAMP_PRINT; DBG_PORT.println(x);  }
+#define TLOGVERBOSEF(...)   if (APP_LOG_LEVEL>4) { TERMINAL_COLOR_VERBOSE; TIMESTAMP_PRINT; DBG_PORT.printf(__VA_ARGS__); }
+#define TLOGVERBOSEF_P(...) if (APP_LOG_LEVEL>4) { TERMINAL_COLOR_VERBOSE; TIMESTAMP_PRINT; DBG_PORT.printf_P(__VA_ARGS__); }
